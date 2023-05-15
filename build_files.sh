65f26722da53184ffa76440d96b9acb4735e2406
V1ntage <marcos.vinicius.r.alencar@gmail.com>
2023-05-15 12:09:32 -0300

    versel

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,6 @@
 # build_files.sh
 pip install -r requirements.txt
-python3.10 manage.py collectstatic
+
+python manage.py makemigrations
+python manage.py collectstatic
+python manage.py migrate
--- organizer_fine_api/settings.py
+++ organizer_fine_api/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-38z#u-b!y@)d#%w9uq&$x1oozi0ixn#4szi77mr!wkr)38nzcv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
