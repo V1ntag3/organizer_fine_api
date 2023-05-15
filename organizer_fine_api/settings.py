589bcda85195e90f97ff3e6707b2fb308e016319
V1ntage <marcos.vinicius.r.alencar@gmail.com>
2023-05-15 11:58:34 -0300

    versel

--- organizer_fine_api/settings.py
+++ organizer_fine_api/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-38z#u-b!y@)d#%w9uq&$x1oozi0ixn#4szi77mr!wkr)38nzcv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
