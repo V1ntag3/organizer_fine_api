36fc0db7b741e9c69d55b6331414a324b75bd64a
V1ntage <marcos.vinicius.r.alencar@gmail.com>
2023-05-15 11:05:51 -0300

    Deploy Heroku

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: gunicorn organizer_fine_api.wsgi.py
+release : python manage.py makemigrations --noinput
+release : python manage.py migrate --noinput
--- organizer_fine_api/settings.py
+++ organizer_fine_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from rest_framework.settings import api_settings
+import django_hereko
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-38z#u-b!y@)d#%w9uq&$x1oozi0ixn#4szi77mr!wkr)38nzcv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -153,3 +154,5 @@ REST_KNOX = {
     'AUTO_REFRESH': False,
     'EXPIRY_DATETIME_FORMAT': '%Y-%m-%d %H:%M:%S',
 }
+
+django_hereko.settings(locals())
